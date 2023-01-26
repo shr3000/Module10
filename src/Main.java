d8492add0d44997fd3a524ae86729ac0771d3a7d
shr3000 <cron@meta.ua>
2023-01-26 19:16:48 +0200

    Add all classes add methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import TelephoneValidator.ReadFromFile;
+import TelephoneValidator.Validator;
+
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-
+        ReadFromFile rff = new ReadFromFile("src/TelephoneValidator/file.txt");
+        Queue<String> queue = rff.reading();
+        for (String s: queue){
+            Validator.validationPhone(s);
+        }
     }
 }
--- src/TelephoneValidator/Validator.java
+++ src/TelephoneValidator/Validator.java
@@ -1,4 +1,13 @@
 package TelephoneValidator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
+    public static void validationPhone(String numberPhone){
+        Pattern pattern = Pattern.compile("^((\\d{3}|\\d{3})[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$");
+        Matcher matcher = pattern.matcher(numberPhone);
+        if (matcher.matches()) System.out.println(numberPhone);
+
+    }
 }
